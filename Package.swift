aff4fc6b2bd8f565cb91bb88da9d99e05079e91b
quanhua.peng <pengquanhua@ifanr.com>
2019-11-11 18:36:01 +0800

    package manager

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "PodLearn",
-            dependencies: ["RecaticeMoya"]),
+            dependencies: ["Moya"]),
         .testTarget(
             name: "PodLearnTests",
             dependencies: ["PodLearn"]),
