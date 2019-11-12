7eb4ef8d435345e0b0c26c479cb2505c9c708881
quanhua.peng <pengquanhua@ifanr.com>
2019-11-12 15:48:22 +0800

    add spm

--- Sources/PodLearn/PodLearn.swift
+++
@@ -1,3 +0,0 @@
-struct PodLearn {
-    var text = "Hello, World!"
-}
--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import PodLearnTests
-
-var tests = [XCTestCaseEntry]()
-tests += PodLearnTests.allTests()
-XCTMain(tests)
