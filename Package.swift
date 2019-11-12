200031fbe9bc0123c42f1ab75c30d9547d61351a
quanhua.peng <pengquanhua@ifanr.com>
2019-11-12 10:39:20 +0800

    add spm

--- Package.swift
+++ Package.swift
@@ -20,6 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "PodLearn",
+            path: "PodLearn",
             dependencies: ["Moya"]),
         .testTarget(
             name: "PodLearnTests",
